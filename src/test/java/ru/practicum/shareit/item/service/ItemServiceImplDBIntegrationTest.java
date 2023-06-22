9c4c2701f4a9bbab014b7ec1bed8e3fe2674b635
DmitriyKrasnikov <dimankrasnikov@mail.ru>
2023-06-22 23:30:50 +0300

    Спринт 15. Правки в тестах 5.

--- src/test/java/ru/practicum/shareit/item/service/ItemServiceImplDBIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/item/service/ItemServiceImplDBIntegrationTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.service;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
